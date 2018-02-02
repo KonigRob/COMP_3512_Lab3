2be562ee1da18d92b40d11224b74bae55d02f6aa
Rob Koenig <rkoenig5@my.bcit.ca>
2018-02-02 12:08:54 -0800

    grabbed food, forgot to push again :p

--- COMP_3512_Lab3/COMP_3512_Lab3/Matrix.hpp
+++ COMP_3512_Lab3/COMP_3512_Lab3/Matrix.hpp
@@ -15,10 +15,21 @@ public:
 	Matrix(int [], int);
 	~Matrix();
 	void set_value(int, int, int);
-	int get_value(int, int);
+	int get_value(int, int) const;
 	void clear();
 	Matrix identity();
 	friend ostream& operator<<(ostream&, const Matrix&);
+	friend bool operator==(const Matrix&, const Matrix&);
+	friend bool operator!=(const Matrix&, const Matrix&);
+	friend bool operator<(const Matrix&, const Matrix&);
+	friend bool operator>(const Matrix&, const Matrix&);
 	friend bool operator<=(const Matrix&, const Matrix&);
+	friend bool operator>=(const Matrix&, const Matrix&);
+	Matrix& operator++();
+	Matrix operator++(int);
+	Matrix& operator--();
+	Matrix operator--(int);
+	friend Matrix operator+(Matrix, const Matrix&);
+	friend Matrix operator-(Matrix, const Matrix&);
 };
 
