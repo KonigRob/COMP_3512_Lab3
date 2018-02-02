476304c98c024db51d917dab608129f3d57dc58b
Rob Koenig <rkoenig5@my.bcit.ca>
2018-02-02 11:21:46 -0800

    I forgot to commit, but am on top of it now!!

--- COMP_3512_Lab3/COMP_3512_Lab3/Matrix.hpp
+++ COMP_3512_Lab3/COMP_3512_Lab3/Matrix.hpp
@@ -1,3 +1,24 @@
 #pragma once
 
+#include <math.h>
+#include <ostream>
+using namespace std;
+
+class Matrix
+{
+	int dimensions;
+	int * arr;
+
+public:
+	Matrix();
+	Matrix(int);
+	Matrix(int [], int);
+	~Matrix();
+	void set_value(int, int, int);
+	int get_value(int, int);
+	void clear();
+	Matrix identity();
+	friend ostream& operator<<(ostream&, const Matrix&);
+	friend bool operator<=(const Matrix&, const Matrix&);
+};
 
